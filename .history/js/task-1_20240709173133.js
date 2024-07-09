4ddb47e499c5ac98ea3a095f6b06397a4e7081c2
ViacheslavParfenenko <143594247+ViacheslavParfenenko@users.noreply.github.com>
2024-07-09 17:56:57 +0100

    HW7

---
+++ .history/js/task-1_20240709173133.js
@@ -0,0 +1,34 @@
+const categoriesList = document.getElementById('categories');
+
+const categoriesItems = categoriesList.querySelectorAll('li.item');
+
+console.log(`Number of categories: ${categoriesItems.length}`);
+
+categoriesItems.forEach(category => {
+  const categoryTitle = category.querySelector('h2').textContent;
+  const categoryItems = category.querySelectorAll('ul li').length;
+  console.log(`Category: ${categoryTitle}`);
+  console.log(`Elements: ${categoryItems}`);
+});
+
+const style = document.createElement('style');
+style.textContent = `
+  .categories {
+    display: flex;
+    flex-wrap:;
+    gap: 10px;
+    list-style: none;
+    border: 1px;
+    padding: 0;
+    margin: 0;
+  }
+  .item {
+    flex: 1 1 calc(33.333% - 10px);
+    box-sizing: border-box;
+  }
+
+  .item-name: 
+
+  .
+  }
+`;
